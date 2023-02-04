65d308f65bbf77200871c3be732a111b9d90c4ff
Alexandre Dias <116679332+AleDiias@users.noreply.github.com>
2023-02-04 17:23:05 -0300

    FilaDeEspera.stories.js
    Stories da parte da fila de espera

---
+++ src/stories/FilaDeEspera.stories.js
@@ -0,0 +1,25 @@
+// import { within, userEvent } from "@storybook/testing-library";
+import MyFilaDeEspera from "../components/FilaDeEspera.vue";
+
+export default {
+  title: "Example/GeneralTable/FilaDeEspera",
+  component: MyFilaDeEspera,
+  parameters: {
+    layout: "fullscreen",
+  },
+};
+
+const Template = (args) => ({
+  components: { MyFilaDeEspera },
+  setup() {
+    return { args };
+  },
+  template: `
+  <my-Fila-De-Espera v-bind="args" />
+  `,
+});
+
+export const FilaDeEspera = Template.bind({});
+FilaDeEspera.args = {
+
+};
